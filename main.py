7f880284ff80c494bc34a57f35d61469b7141d13
Shinjan <mshinjan@iisc.ac.in>
2020-08-30 19:23:42 +0530

    Printing DUT Resistance

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import matplotlib
 matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
 
-CONVERGENCE_THRESHOLD = 1e-20
+CONVERGENCE_THRESHOLD = 1e-8
 
 class Fixed:
     FREE = 0
     A = 1
     B = 2
+    C = 3
+    D = 4
 
 class Node:
     __slots__ = ["voltage","fixed"]
@@ -17,10 +19,11 @@ class Node:
         self.fixed = f
 
 def set_boundary(grid1,grid2):
-    for j in range(2):
-        grid1[j][0] = Node(10.0, Fixed.A)
-    for j in range(1):
-        grid2[j][0] = Node(0.0, Fixed.B)
+    grid1[8][0] = Node(10.0, Fixed.A)
+    grid2[3][0] = Node(0.0, Fixed.B)
+    grid2[7][0] = Node(0.0, Fixed.B)
+    grid1[3][34] = Node(0.0, Fixed.C)
+    grid2[3][29] = Node(0.0, Fixed.D)
 
 
 def find_cross_boundaries(row1, col1, row2, col2):
@@ -52,7 +55,7 @@ def V_mnp(m,n,p,grid1,grid2,row1,col1,row2,col2,R_top,R_bot,R_cross,cross_bounda
     Vmnp = 0
     R_inv = 0.0
     if p==0:
-        if grid1[m][n].fixed == Fixed.FREE:
+        if (grid1[m][n].fixed == Fixed.FREE) or (grid1[m][n].fixed == Fixed.C) or (grid1[m][n].fixed == Fixed.D):
             if m!=0:
                 Vmnp += grid1[m-1][n].voltage / R_top
                 R_inv += 1/R_top
@@ -76,7 +79,7 @@ def V_mnp(m,n,p,grid1,grid2,row1,col1,row2,col2,R_top,R_bot,R_cross,cross_bounda
         else:
             diff = 0
     if p==1:
-        if grid2[m][n].fixed == Fixed.FREE:
+        if (grid2[m][n].fixed == Fixed.FREE) or (grid2[m][n].fixed == Fixed.C) or (grid2[m][n].fixed == Fixed.D):
             if m!=0:
                 Vmnp += grid2[m-1][n].voltage / R_bot
                 R_inv += 1/R_bot
@@ -144,7 +147,7 @@ def iterations(number_of_iterations, grid1, grid2, cross_boundary, R_top, R_bot,
         for m in range(row2):
             for n in range(col2):
                 diff += V_mnp(m,n,p,grid1,grid2,row1,col1,row2,col2,R_top,R_bot,R_cross,cross_boundary)
-        if (i+1)%500==0:
+        if (i+1)%1000==0:
             print("Step {} with convergence delta = {}".format(i+1,diff))
         if diff<CONVERGENCE_THRESHOLD:
             print("Step {} with convergence delta = {} has converged".format(i+1,diff))
@@ -155,44 +158,53 @@ def iterations(number_of_iterations, grid1, grid2, cross_boundary, R_top, R_bot,
 def resistance(num_iter, grid1, grid2, cross, R_top, R_bot, R_cross):
     iterations(num_iter, grid1, grid2, cross, R_top, R_bot, R_cross)
     I1, I2 = current_grid(grid1,grid2,cross,R_bot,R_top,R_cross)
-    curA, curB = 0.0, 0.0
+    curA, curB, volC, volD = 0.0, 0.0, 0.0, 0.0
     for i in range(np.shape(grid1)[0]):
         for j in range(np.shape(grid1)[1]):
             if grid1[i][j].fixed == Fixed.A: curA += I1[i][j]
             if grid1[i][j].fixed == Fixed.B: curB += I1[i][j]
+            if grid1[i][j].fixed == Fixed.C: volC = grid1[i][j].voltage
+            if grid1[i][j].fixed == Fixed.D: volD = grid1[i][j].voltage
     for i in range(np.shape(grid2)[0]):
         for j in range(np.shape(grid2)[1]):
             if grid2[i][j].fixed == Fixed.A: curA += I2[i][j]
             if grid2[i][j].fixed == Fixed.B: curB += I2[i][j]
+            if grid2[i][j].fixed == Fixed.C: volC = grid2[i][j].voltage
+            if grid2[i][j].fixed == Fixed.D: volD = grid2[i][j].voltage
     cur = curA - curB
     res = 10/cur
-    return res, curA, curB
+    return res, curA, curB, volC, volD
 
 
-row1 = 5
-col1 = 10 
-row2 = 5
-col2 = 10
+row1 = 10
+col1 = 35
+row2 = 18
+col2 = 30
 
 R_top   = 10
-R_bot   = 15
+R_bot   = 12
 R_cross = 10
 
 print("--------------------------------------------- ")
 print("| Layer 1: {} X {}".format(row1,col1))
 print("| Layer 2: {} X {}".format(row2,col2))
-print("| R_top: {} Ohms \t R_bot: {} \t R_cross: {} Ohms".format(R_top,R_bot,R_cross))
+print("| R_top: {} Ohms \t R_bot: {} Ohms \t R_cross: {} Ohms".format(R_top,R_bot,R_cross))
 print("| Convergence Threshold: ", CONVERGENCE_THRESHOLD)
 print("--------------------------------------------- \n")
 layer1 = [[Node() for i in range(col1)] for j in range(row1)]
 layer2 = [[Node() for i in range(col2)] for j in range(row2)]
 set_boundary(layer1,layer2)
 cross = find_cross_boundaries(row1, col1, row2, col2)
-print(cross)
-final_resistance, curA, curB = resistance(10000, layer1, layer2, cross, R_top, R_bot, R_cross)
+
+final_resistance, curA, curB, Vplus, Vminus = resistance(500000, layer1, layer2, cross, R_top, R_bot, R_cross)
+
 print("\nThe resistance of the network is %.5f Ohms."%final_resistance)
 print("I+ = ", curA)
 print("I- = ", curB)
+print("V+ = ", Vplus)
+print("V- = ", Vminus)
+print("DUT Resistance = ", (Vplus-Vminus)/curA)
+
 V1, V2 = np.empty((row1,col1)), np.empty((row2,col2))
 for i in range(row1):
     for j in range(col1):
@@ -204,8 +216,8 @@ for i in range(row2):
 X1 = [i for i in range(col1)]
 Y1 = [i for i in range((col2-row1)//2+row1,(col2-row1)//2,-1)]
 x1,y1 = np.meshgrid(X1,Y1)
-#X2 = [i for i in range((col1-row2)//2,(col1-row2)//2+row2)]
-X2 = [i for i in range(col1,col1+row2)]
+spacing = 10
+X2 = [i for i in range(col1+spacing,col1+row2+spacing)]
 Y2 = [i for i in range(col2)]
 x2,y2 = np.meshgrid(X2,Y2)
 
@@ -214,6 +226,10 @@ plt.contourf(x1,y1,V1,levels,alpha=0.8,cmap='twilight_shifted')
 plt.contourf(x2,y2,V2.T,levels,alpha=0.8,cmap='twilight_shifted')
 plt.colorbar()
 plt.axis('off')
-plt.title("Voltage Distribution")
-plt.show()
-#plt.savefig('Voltage_Distribution.png')
+plt.text(-5.3, 16.5, r'I$^+$', fontsize=25)
+plt.text(46.5, -2.5, r'I$^-$', fontsize=25)
+plt.text(34.5,14,r'V$^+$', fontsize=25)
+plt.text(49, 30.3, r'V$^-$', fontsize=25)
+#plt.title("Voltage Distribution")
+#plt.show()
+plt.savefig('Voltage_Distribution.png')
