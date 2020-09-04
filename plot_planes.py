362ec9faaba59952d93973a16634b7d98356e513
Shinjan <mshinjan@iisc.ac.in>
2020-09-04 14:52:24 +0530

    modified:   plot_planes.py

--- plot_planes.py
+++ plot_planes.py
@@ -2,10 +2,19 @@ import matplotlib
 matplotlib.use('tkagg')
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 from parse_input import parse
 
 row1, col1, row2, col2, R_top, R_bot, R_cross_range, Ip_layer, Ip_pos1, Ip_pos2, Im_layer, Im_pos1, Im_pos2, Vp_layer, Vp_pos1, Vp_pos2, Vm_layer, Vm_pos1, Vm_pos2, CONVERGENCE_THRESHOLD, max_iter,append = parse('input.dat')
+
+def find_cross_boundaries(row1, col1, row2, col2):
+    L2l = (col2 - row1)//2
+    L2r = L2l + row1 - 1
+    L1l = (col1 - row2)//2
+    L1r = L1l + row2 - 1
+    cross = [L1l, L1r, L2l, L2r]
+    return cross
 
 
 for R_cross in R_cross_range:
@@ -27,7 +36,8 @@ for R_cross in R_cross_range:
     X2 = [i for i in range(col1+spacing,col1+row2+spacing)]
     Y2 = [i for i in range(col2)]
     x2,y2 = np.meshgrid(X2,Y2)
-
+    
+    cross_boundary = find_cross_boundaries(row1, col1, row2, col2)
     levels = np.arange(-0.015,10.015,0.005)+0.005
     if Ip_layer==1:
         plt.text(Ip_pos2-8,(col2-row1)//2+row1-Ip_pos1,r'I$^+$',fontsize=25)
@@ -53,8 +63,12 @@ for R_cross in R_cross_range:
         ax2.text(Vm_pos1+spacing+col1,Vm_pos2,r'V$^-$',fontsize=25)
         ax2.plot(Vm_pos1+spacing+col1,Vm_pos2,'ro',c='k')
     ax2.contourf(x1,y1,V1,levels,alpha=0.8,cmap='hsv',antialiased=True)
+    rect1 = patches.Rectangle(( cross_boundary[0], (col2-row1)//2+1),cross_boundary[1]-cross_boundary[0],row1-1,linewidth=1,edgecolor='k',facecolor='None')
+    ax2.add_patch(rect1)
     ax2.contourf(x2,y2,V2.T,levels,alpha=0.8,cmap='hsv',antialiased=True)
-    ax2.axis('off')
+    rect2 = patches.Rectangle((spacing+col1,(col2-row1)//2+1),row2-1,cross_boundary[3]-cross_boundary[2],linewidth=1,edgecolor='k',facecolor='None')
+    ax2.add_patch(rect2)
+    #ax2.axis('off')
     ax2.set_ylim(0,col2+1)
     ax2.set_xlim(0,col1+row2+spacing+5)
     ax2.set_aspect('equal')
